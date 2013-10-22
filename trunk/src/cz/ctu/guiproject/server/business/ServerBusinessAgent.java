9783c279d25cbb8fd0726bc235f7e37ea480254c
tomasbukcz@gmail.com <tomasbukcz@gmail.com@073d2b55-ec7a-4d6e-2fd1-0b164817ed8a>
2013-10-22 13:59:40 +0000

    UPDATE#REFACTOR: several updates[TBU]

--- trunk/src/cz/ctu/guiproject/server/business/ServerBusinessAgent.java
+++ trunk/src/cz/ctu/guiproject/server/business/ServerBusinessAgent.java
@@ -4,10 +4,30 @@
  */
 package cz.ctu.guiproject.server.business;
 
+import cz.ctu.guiproject.server.observers.EventObserver;
+
 /**
  *
  * @author tomas.buk
  */
 public interface ServerBusinessAgent {
-    
+
+    /**
+     * Registers new EventObserver instance
+     *
+     * @param o new EventObserver instance
+     */
+    public void registerObserver(EventObserver o);
+
+    /**
+     * Removes particular EventObserver instance
+     *
+     * @param o EventObserver instance to be removed
+     */
+    public void removeObserver(EventObserver o);
+
+    /**
+     * Notifies all registered observers, that particular event occured
+     */
+    public void notifyEventObservers();
 }
--- trunk/src/cz/ctu/guiproject/server/main/AndroidServer.java
+++ trunk/src/cz/ctu/guiproject/server/main/AndroidServer.java
@@ -4,10 +4,50 @@
  */
 package cz.ctu.guiproject.server.main;
 
+import cz.ctu.guiproject.server.observers.ClickObserver;
+import cz.ctu.guiproject.server.observers.TouchObserver;
+
 /**
  *
  * @author tomas.buk
  */
 public interface AndroidServer {
-    
+
+    /**
+     * Registers new TouchObserver instance
+     *
+     * @param o new TouchObserver instance
+     */
+    public void registerTouchObserver(TouchObserver o);
+
+    /**
+     * Removes TouchServer instance
+     *
+     * @param o new TouchObserver instance
+     */
+    public void removeTouchObserver(TouchObserver o);
+
+    /**
+     * Notify all registered observers, that new TouchEvent occured
+     */
+    public void notifyTouchObservers();
+
+    /**
+     * Registers new ClickObserver instance
+     *
+     * @param o new ClickObserver instance
+     */
+    public void registerClickObserver(ClickObserver o);
+
+    /**
+     * Removes ClickObserver instance from the list of registered observers
+     *
+     * @param o ClickObserver instance to be removed
+     */
+    public void removeClickObserver(ClickObserver o);
+
+    /**
+     * Notify all registered observers, that the ClickEvent Occured
+     */
+    public void notifyClickObserver();
 }
